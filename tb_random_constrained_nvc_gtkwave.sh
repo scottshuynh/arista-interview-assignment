b266082227b65bfa7005ebba203fa6e17efbc058
Scott Huynh <scott.s.huynh@gmail.com>
2025-08-18 20:08:40 +0930

    Add gtkwave wave option to tb_random_constrained_nvc_gtkwave.sh

--- tb_random_constrained_nvc_gtkwave.sh
+++ tb_random_constrained_nvc_gtkwave.sh
@@ -2,4 +2,4 @@
 if [ ! -f compile_order.txt ]; then
     hdldepends hdldepends_config.toml --top-entity tb_vhdl_assignment_simple --compile-order-vhdl-lib work:compile_order.txt
 fi
-hdlworkflow nvc top compile_order.txt --cocotb tb_vhdl_assignment
+hdlworkflow nvc top compile_order.txt --cocotb tb_vhdl_assignment --wave gtkwave
