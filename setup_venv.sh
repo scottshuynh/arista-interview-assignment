f4dda643609478e8ec403a3aba8be117c0073fb1
Scott Huynh <scott.s.huynh@gmail.com>
2025-08-18 19:50:30 +0930

    Fix typo. hdldepends install with working git hash

--- setup_venv.sh
+++ setup_venv.sh
@@ -3,9 +3,9 @@ if [ ! -f venv/bin/activate ]; then
     echo "No venv found. Making one now..."
     python -m venv venv
     source venv/bin/activate
-    pip install git+https://github.com/pevhall/hdldepends.git
+    pip install git+https://github.com/pevhall/hdldepends.git@5b44ba5
     pip install git+https://github.com/scottshuynh/hdlworkflow.git
     pip install cocotb
 else
-    echo "venv found. Aborting...
+    echo "venv found. Aborting..."
 fi
